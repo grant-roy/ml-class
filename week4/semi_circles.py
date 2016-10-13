8952f852c2039319c3c996074db0b9a7f949e2a6
Erin <grant.m.roy@gmail.com>
2016-10-12 22:20:07 -0700

    complete data creation for semi_circles.py

--- week4/semi_circles.py
+++ week4/semi_circles.py
@@ -6,59 +6,50 @@ import numpy as np
 # our function to actually create the points on the x,y plane
 # that creates a semi-circle
 def xy(r, x_scaler=0, y_scaler=0, sign=1):
-    phi = sign * np.arange(0, 3.14, 0.01)
+    phi = sign * np.arange(0, 3.14, .001)
 
     return pd.DataFrame(data={'x':r*np.cos(phi)+x_scaler,
                               'y':r*np.sin(phi)+y_scaler})
 
-    #return r*np.cos(phi)+x_scaler, r*np.sin(phi)+y_scaler
-
 # we would like a semi-circle arc of a particular width. We
 # specify the starting and ending radius, along with the decrement
 # step as the third argument
-semi_circle_width = np.arange(1., .89, -.01)
 columns = ['x', 'y']
 red = pd.DataFrame(data=np.zeros((0, len(columns))), columns=columns)
-blue  = red.copy(deep=True)
+blue = red.copy(deep=True)
 
 # generate points on an x,y plane that will form a positive and negative
 # semi-circle. 'red' will be like the St. Louis arch, and 'blue' will be flipped
 # upside down.
-for index, radius_step in enumerate(semi_circle_width):
-
-    #red_x, red_y = xy(radius_step, y_scaler=.5)
-    #xy(radius_step, y_scaler=.5)
+for radius_step in np.arange(1., .89, -.001):
     red = pd.concat((red, xy(radius_step, y_scaler=.5)), axis=0)
     blue = pd.concat((blue, xy(radius_step, x_scaler=1, sign=-1)), axis=0)
-    #print red
-    #red['x'] = red_x
-    #print red.tail()
-    #red['y'] =red_y
-    #blue.ix['x'], blue.loc['y'] = xy(radius_step, x_scaler=1, sign=-1)
-print red.reset_index()
-#plt.plot(*red[index], c='r', ls='-')
-#plt.plot(*blue[-1], c='b', ls='-')
 
+# we will now sample 2000 random points from each of our semi-circle dataframes
+red_samples = red.reset_index(drop=True).loc[np.random.choice(red.index, 2000)]
+blue_samples = blue.reset_index(drop=True).loc[np.random.choice(blue.index, 2000)]
 
-# here we are going to generate random points on an x,y plane. Some of these points
-# will fall within our semicircle, others will not
-#rand_x = [round(np.random.uniform(low=-1.0, high=2.0), 2) for _ in xrange(100)]
-#rand_y = [round(np.random.uniform(low=-1.0, high=2.0), 2) for _ in xrange(100)]
-#rand_coordinates = zip(rand_x, rand_y)
+# assign classification to each semicircle
+red_samples['classification'] = pd.Series([1] * 2000, index=red_samples.index)
+blue_samples['classification'] = pd.Series([-1] * 2000, index=blue_samples.index)
 
-#for index, item in enumerate(semi_circle_width):
-   # print red[index]
+all_samples = pd.concat([red_samples, blue_samples], axis=0).reset_index(drop=True)
+all_samples['scalar'] = pd.Series([1] * len(all_samples), index=all_samples.index)
+print all_samples.head()
+print all_samples.tail()
+#print pd.concat([red_samples, pd.Series([1] * 2000, name='scalar')], join='inner', axis=1)
 
-#rand_x = [round(np.random.uniform(low=-1.0, high=2.0), 2) for _ in xrange(100)]
-#rand_y = [round(np.random.uniform(low=-1.0, high=2.0), 2) for _ in xrange(100)]
-#rand_coordinates = zip(rand_x, rand_y)
-#plt.scatter(rand_coordinates,c='g')
-# print rand_coordinates
+# next create a full dataset from semi-circles with 'y' values for classification
+# this will allow us to match the three columns our weight vector will have
+#all_samples = pd.concat((red_samples, blue_samples), axis=0)
+#print all_samples.reset_index(drop=True).tail()
 
-# might be useful for generating random separating lines
-#rand_x = np.random.uniform(low=-1.0, high=2.0, size=(1, 1))
-#rand_y = np.random.uniform(low=-1.0, high=1.5, size=(1, 1))
-#rand_coordinates = zip(rand_x, rand_y)
-
-
-#plt.show()
+# scalar = pd.Series([1] * len(coin_data), name='scalar')
+#
+# # create our x vector with the added scalar column
+# x = pd.concat((scalar, coin_data.loc[:, ['size', 'mass']]), axis=1)
+#
+# # plot red and blue semicircles from sampled points
+# plt.scatter(red_samples['x'], red_samples['y'], c='r')
+# plt.scatter(blue_samples['x'], blue_samples['y'], c='b')
+# plt.show()
