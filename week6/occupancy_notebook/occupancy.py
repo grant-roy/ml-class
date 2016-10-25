97b0372c972d9ac558d017fb114285d674d8f4eb
Erin <grant.m.roy@gmail.com>
2016-10-25 12:14:54 -0700

    initialize vc representation

--- week6/occupancy_notebook/occupancy.py
+++ week6/occupancy_notebook/occupancy.py
@@ -1,10 +1,8 @@
 import pandas as pd
 import numpy as np
 
-import sys
-
-sys.path.append('../../utility')
 import pocket as pk
+import vc_analysis as vc
 
 
 occupancy_data = pd.read_csv('datatraining.txt', header=0)
@@ -13,4 +11,5 @@ del occupancy_data['date']
 # we need to convert these digits 1 and 5 into a binary signal [1,-1]
 occupancy_data['Classification'] = occupancy_data['Classification'].apply(lambda value: value if value==1 else -1)
 
-print pk.pocket(occupancy_data)
+result = pk.pocket(occupancy_data, iterations=30)
+vc = vc.misclassified_count(w=result['w'], data=occupancy_data)
---
+++ week6/occupancy_notebook/vc_analysis.py
@@ -0,0 +1,25 @@
+from __future__ import division
+import pandas as pd
+import numpy as np
+import math
+
+
+
+def misclassified_count(w, data):
+    scalar = pd.Series([1] * (len(data)+1), name='scalar')
+    y = data['Classification']
+    data['scalar'] = scalar
+    x = data.drop(['Classification'], axis=1)
+
+    misclassified = 0.
+    for index, row in x.iterrows():
+
+        y_hat = np.dot(w, row.T, out=None)
+
+        if np.sign(y[index]) != np.sign(y_hat):
+            misclassified += 1.
+    return {'misclassified':misclassified, 'ratio_misclassified':misclassified/len(data)}
+
+
+def vc_bound(N=1, tolerance=.14, mH=1):
+    return math.sqrt( (8/N) * math.log((4 * 2 * mH)/tolerance ) )
