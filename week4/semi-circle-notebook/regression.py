80824a0dee7f3e89f05896a76e14d7acc1b6fd86
protocol <protocol@protocols-mbp.lan>
2021-05-22 14:14:19 -0700

    update python for week4 notebooks

--- week4/semi-circle-notebook/regression.py
+++ week4/semi-circle-notebook/regression.py
@@ -4,11 +4,11 @@ import scatter_plot as sp
 from matplotlib import pyplot as plt
 
 def regression(data):
-    X = data.as_matrix()
+    X = data.values
 
     # linear algebra with numpy
     A = np.vstack([X[:, 0], np.ones(len(X))]).T
-    m, c = np.linalg.lstsq(A, X[:, 1])[0]
+    m, c = np.linalg.lstsq(A, X[:, 1],rcond=-1)[0]
 
     return [m,c]
 
@@ -17,6 +17,6 @@ def plot_regression(data,m,c):
 
     plt.scatter(data['x'], data['y'], marker='+')
 
-    X = data.as_matrix()
+    X = data.values
     plt.plot(X[:, 0], m*X[:, 0] + c, 'r', label='Fitted line')
     plt.show()
